71851ead40c44fdbfc26405a69f7f16c050cad85
Nick Youngblut <nyoungb2@illinois.edu>
2013-01-05 12:02:44 -0600

    proc_track: output edit

--- bin/proc_track.pl
+++ bin/proc_track.pl
@@ -11,12 +11,13 @@ use File::Spec;
 ### args/flags
 pod2usage("$0: No files given.") if ((@ARGV == 0) && (-t STDIN));
 
-my ($verbose, $ntime, $pid, $email, $cmd);
+my ($verbose, $ntime, $pid, $email, $cmd, $outfile);
 GetOptions(
 	   "step=i" => \$ntime,
 	   "proc=s" => \$pid,			# pid to track
 	   "mail=s" => \$email,			
 	   "command=s" => \$cmd, 		# command
+	   "output=s" => \$outfile,
 	   "verbose" => \$verbose,
 	   "help|?" => \&pod2usage # Help
 	   );
@@ -41,7 +42,7 @@ if(! $pid){		# if running command
 	my $pid = fork();
 	if($pid){		# if parent
 		push(@pids, $pid);
-		follow_pid($pid);
+		follow_pid($pid, $outfile);
 		}
 	else{			# if child
 		`$cmd`;
@@ -49,16 +50,20 @@ if(! $pid){		# if running command
 		}
 	foreach my $child (@pids){
 		my $pid = waitpid($child, 0);
-		print "$pid finished\n";
+		#print "$pid finished\n";
 		}
 	}
 else{		# if following a pid provided pid
-	follow_pid($pid);
+	follow_pid($pid, $outfile);
 	}
 
 # follow pid #
 sub follow_pid{
-	my ($pid) = @_;
+	my ($pid, $outfile) = @_;
+	
+	$outfile = "PID$pid.log" if ! $outfile;
+	open OUT, ">>$outfile" or die $!;
+	
 	while(1){
 		my $out = `ps aux | egrep "^[a-zA-Z0-9]+ +$pid +"`;
 		$out =~ s/ +/\t/g;
@@ -71,13 +76,16 @@ sub follow_pid{
 
 			# notification #	
 			`echo "PID: $pid done\nFollowed the job for $duration seconds." | mailx -s "$0 job done" $email` if $email;
-			print STDERR "$pid job done\nFollowed the job for $duration seconds\n";
+			print STDERR "PID: $pid job done\nFollowed the job for $duration seconds\n";
 			last;
 			}
-		print join("\n", @out), "\n";	
+
+		print OUT join("\n", @out), "\n";	
 		
 		sleep $ntime;
 		}
+	
+	close OUT;
 	}
 	
 
@@ -93,11 +101,11 @@ proc_track.pl -- Tracking of 'ps aux' for a process.
 
 =head2 Provide a command
 
-proc_track.pl [-s] [-m] -c > proc.log
+proc_track.pl [-s] [-m] [-o] -c
 
 =head2 Provide a PID
 
-proc_track.pl [-s] [-m] -p > proc.log
+proc_track.pl [-s] [-m] [-o] -p
 
 =head2 options
 
@@ -111,7 +119,7 @@ proc_track.pl [-s] [-m] -p > proc.log
 
 =item -m 	Email for a notification when the PID is finished.
 
-=item -v	Verbose output
+=item -o 	Output file name. [PID#.log]
 
 =item -h	This help message
 
@@ -127,15 +135,17 @@ Continually log 'ps aux' for a process. If a command is executed,
 this script will fork a child process and execute the command; the
 parent will then follwo the child process.
 
+Output is appended to old output if file already exists.
+
 =head1 EXAMPLES
 
 =head2 Providing a command
 
-proc_track.pl -m nyoungb2@illinois.edu -c "sleep 120" > cmd.log
+proc_track.pl -m nyoungb2@illinois.edu -s 10 -c "sleep 60"
 
 =head2 Providing a PID
 
-proc_track.pl -m nyoungb2@illinois.edu -p 1214 > pid1214.log
+proc_track.pl -m nyoungb2@illinois.edu -p 1214
 
 =head1 AUTHOR
 
