03117bb6af57359454f5515099d3b165d1c919c4
Nick Youngblut <nyoungb2@illinois.edu>
2013-06-13 09:34:54 -0500

    fasta2NewNexus: bug fix

--- blib/script/fasta2NewNexus.pl
+++ blib/script/fasta2NewNexus.pl
@@ -71,7 +71,7 @@ sub fasta2newnexus{
 	
 	# Matrix #
 	print "\tMATRIX\n";
-	foreach my $taxon (@taxa){
+	foreach my $taxon (keys %$fasta_r){
 		print join("\t\t", $taxon, $fasta_r->{$taxon}), "\n";
 		}
 	print ";\nEND;\n\n";
